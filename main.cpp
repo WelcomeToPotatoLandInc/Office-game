99ae11f0cd19de5f9b8316892f31ad8f116f9642
WelcomeToPotatoLandInc <thewolfington@gmail.com>
2017-01-28 21:59:26 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,7 +6,11 @@ using namespace std;
 long long workers=0,janitors=0,floors=0,totalfund=7500,profit=0,workercapacity;
 int workersavaliableforhire;
 long long day=1;
-bool evadingtax;
+bool evadingtax=false;
+int chanceofgettingcaught;
+bool caught=false;
+bool painting=false,flower=false,fancyflower=false,watercooler=false;
+bool paintingbought=false,flowerbought=false,fancyflowerbought=false,watercoolerbought=false;
 void buying(){
     cout << "Workers avaliable for hire: " << workersavaliableforhire << endl;
     cout << "Worker - 200$ initially, 100$ for wages" << endl << "Janitor - 100$ initially, 25$ for wages." << endl << "Floor - 250$, 100$ for upkeep" << endl;
@@ -25,7 +29,8 @@ void buying(){
             cout << "You just bought a worker!" << endl;
             workers++;
             workersavaliableforhire--;
-            totalfund=totalfund-200;}
+            totalfund=totalfund-200;
+            }
         }
     if(answer=="2"){
         if(janitors==floors){
@@ -35,7 +40,7 @@ void buying(){
             cout << "You bought a janitor." << endl;
             janitors++;
             totalfund=totalfund-100;
-    }
+        }
 }
     if(answer=="3"){
         cout << "You just bought a floor." << endl;
@@ -46,11 +51,43 @@ void buying(){
     if(answer=="exit" || answer=="Exit"){
         return;
     }
-    else if(answer!="1" && answer!="2" && answer!="3" && answer!="exit" && answer!="Exit"){
+    else if(answer!="1" && answer!="2" && answer!="3" && answer!="exit" && answer!="Exit" && answer!="4" && answer!="5" && answer!="6" && answer!="7"){
         cout << "Error. Unknown input." << endl;
     }
-
+    if(painting==true){
+        cout << "4. Painting" << endl;
     }
+    if(flower==true){
+        cout << "5. Flower" << endl;
+    }
+    if(fancyflower==true){
+        cout << "6. Fancy flower" << endl;
+    }
+    if(watercooler==true){
+        cout << "7. Water cooler" << endl;
+    }
+    if(answer=="4" && day>5 && totalfund>10000 && painting==true){
+        paintingbought==true;
+        cout << "Painting bought!" << endl;
+        painting=false;
+    }
+    if(answer=="5" && day>10 && totalfund>25000 && flower==true){
+        flowerbought=true;
+        cout << "Flower bought!" << endl;
+        flower==false;
+    }
+    if(answer=="6"  && day>20 && totalfund>35000 && fancyflower==true){
+        cout << "Fancy flowers bought!" << endl;
+        fancyflowerbought=true;
+        fancyflower=false;
+    }
+    if(answer=="7" && day>25 && totalfund>27500 && watercooler==true){
+        cout << "Water cooler bought!" << endl;
+        watercoolerbought=true;
+        watercooler=false;
+    }
+    }
+
 }
 int winlose(){
     if(totalfund>100000 && floors>50){
@@ -68,6 +105,34 @@ void results(){
         cout << "Not all floors have janitors and therefor are dirty. Worker productivity drops." << endl;
         profit=profit-workers*50;
     }
+    if(evadingtax=true){
+        srand(time(NULL));
+        chanceofgettingcaught=rand() % 100 + 1;
+        if(chanceofgettingcaught>50){
+            cout << "Uh oh. You got caught evading taxes and are under arrest. G A M E O V E R" << endl;
+            caught=true;
+            return;
+        }
+        else if(chanceofgettingcaught<50 || chanceofgettingcaught==50){
+            profit=profit*2;
+        }
+    }
+    if(paintingbought==true){
+        cout << "The painting doubled your profits, because workers enjoy the office more." << endl;
+        profit=profit*2;
+    }
+    if(flowerbought==true){
+        cout << "Flowers make the office better. Two times the income." << endl;
+        profit=profit*2;
+    }
+    if(fancyflowerbought==true){
+        cout << "People enjoy the fancy flowers. Profit tripled!" << endl;
+        profit=profit*3;
+    }
+    if(watercoolerbought==true){
+        cout << "Workers like cold, fresh water. Profits doubled!" << endl;
+        profit=profit*2;
+    }
     cout << "You made " << profit << endl;
     totalfund=(totalfund+profit)-(workers*100+janitors*25+floors*100);
     cout << "Your total fund is, after paying your workers wages " << totalfund << endl;
@@ -80,26 +145,50 @@ void results(){
 }
 void workersforhire(){
     srand(time(NULL));
-    workersavaliableforhire=rand() % 10 + 1;}
+    workersavaliableforhire=rand() % 10 + 1;
+    }
 void taxevasion(){
-    string evadingtaxes;
-    cout << "Do you want to evade taxes? [Y/N] (Evading taxes doubles all profits, however there is a 50 50 chance you will get caught" << endl;
-    cin >> evadingtaxes;
-    if(evadingtaxes=="y" || evadingtaxes=="Y"){
+    string evading;
+    cout << "Do you want to attempt evading taxes? You have a 50 50 chance of getting caught, but it doubles your profits. [Y/N]" << endl;
+    cin >> evading;
+    if(evading=="Y" || evading=="y"){
         evadingtax=true;
     }
-    else{
+    else if(evading=="n" || evading=="N"){
         evadingtax=false;
+    }
+
+    }
+void decorations(){
+    if(day>5 && totalfund>10000){
+        cout << "First decoration unlocked! Buy it for 5000$. Its a painting that boosts morale and workers give twice as much." << endl;
+        painting=true;
+    }
+    if(day>10 && totalfund>25000){
+        cout << "Second decoration unlocked! Rose flowers make the office smell better and look better. Workers give twice the income. 25000$" << endl;
+        flower=true;
+    }
+    if(day>20 && totalfund>35000){
+        cout << "Third decoration unlocked! Fancy flowers. Office looks better. Workers give 3 times the profit. 35000$" << endl;
+        fancyflower=true;
+    }
+    if(day>25 && totalfund>27500){
+        cout << "Water coolers unlocked! Workers do more work because of proper hydration. 27500$ to buy." << endl;
+        watercooler=true;
     }
 }
 int main()
 {
     while(true){
         workersforhire();
+        decorations();
         buying();
         winlose();
         taxevasion();
         results();
+        if(caught==true){
+            exit(EXIT_SUCCESS);
+        }
         string stayorexit;
         cout << "Press enter to continue the game. Type exit to leave, or continue to go on." << endl;
         cin >> stayorexit;
